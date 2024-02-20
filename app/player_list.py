985dcad2a3a2ddb2fdea71c7f442361aa317c8e4
Scott <winroof1988@gmail.com>
2024-02-20 16:55:43 +0800

    Step 6 done, insertion at tail method and two tail test cases added

--- app/player_list.py
+++ app/player_list.py
@@ -3,11 +3,12 @@ from .player_node import PlayerNode
 
 class PlayerList:
     def __init__(self):
-        self.__head = None
-    '''
-    if head is none, list is empty
-    if not, list is not empty 
-    '''
+        self.__head = None  #if head is none, list is empty
+        self.__tail = None
+
+    @property
+    def tail(self):
+        return self.__head
 
     def is_empty(self):
         return self.__head is None
@@ -19,7 +20,20 @@ class PlayerList:
         new_node = PlayerNode(player)
         if self.is_empty():
             self.__head = new_node
+            self.__tail = new_node
         else:
             new_node.next_node = self.__head
             self.__head.prev_node = new_node
             self.__head = new_node
+
+    def insert_at_tail(self, player):
+        new_node = PlayerNode(player)
+        if self.is_empty():
+            self.__head = new_node
+            self.__tail = new_node
+        else:
+            self.__tail.next_node = new_node
+            new_node.prev_node = self.__tail
+            self.__tail = new_node
+
+
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -2,6 +2,7 @@ import unittest
 from app.player_list import PlayerList
 from app.player import Player
 
+
 class TestPlayerList(unittest.TestCase):
     def test_insert_at_head_empty_list(self):
         player_list = PlayerList()
@@ -19,6 +20,23 @@ class TestPlayerList(unittest.TestCase):
         self.assertFalse(player_list.is_empty())
         self.assertEqual(player_list._PlayerList__head.player, player2)
         self.assertEqual(player_list._PlayerList__head.next_node.player, player1)
+
+    def test_insert_at_tail_empty_list(self):
+        player_list = PlayerList()
+        player = Player(35, "Scott")
+        player_list.insert_at_tail(player)
+        self.assertFalse(player_list.is_empty())
+        self.assertEqual(player_list._PlayerList__tail.player, player)
+
+    def test_insert_at_tail_not_empty_list(self):
+        player_list = PlayerList()
+        player1 = Player(35, "Scott")
+        player2 = Player(30, "Emily")
+        player_list.insert_at_tail(player1)
+        player_list.insert_at_tail(player2)
+        self.assertFalse(player_list.is_empty())
+        self.assertEqual(player_list._PlayerList__tail.player, player2)
+        self.assertEqual(player_list._PlayerList__tail.prev_node.player, player1)
 
 
 if __name__ == '__main__':
