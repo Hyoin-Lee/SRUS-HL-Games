b2d336483855fa456b1ed9bfbfe97a3f5352f52b
winroof1988@gmail.com <Qlalf2ek!@#$>
2024-03-09 21:34:32 +0800

    Portfolio Assessment Task 2 Step 5 argon2-cffi package, Player class, Test cases updated

--- app/player.py
+++ app/player.py
@@ -1,7 +1,12 @@
+from argon2 import PasswordHasher
+
+
 class Player:
     def __init__(self, uid, name):
         self.__uid = uid
         self.__name = name
+        self._hash_password = None
+        self.ph = PasswordHasher()
 
     @property
     def uid(self):
@@ -14,3 +19,15 @@ class Player:
     @classmethod
     def __str__(self):
         return f"Player name is {self.__name} and user ID is {self.__uid}"
+
+    def add_password(self, plaintext_password):
+        if not isinstance(plaintext_password, str):
+            raise TypeError("Password must be string")
+        self._hash_password = self.ph.hash(plaintext_password)
+
+    def verify_password(self, plaintext_password):
+        try:
+            self.ph.verify(self._hash_password, plaintext_password)
+            return True
+        except Exception:
+            return False
--- test/player_test.py
+++ test/player_test.py
@@ -1,5 +1,6 @@
 import unittest
-from app.player import Player
+from player import Player
+
 
 class TestPlayer(unittest.TestCase):
     def test_uid(self):
@@ -9,6 +10,28 @@ class TestPlayer(unittest.TestCase):
     def test_name(self):
         player = Player("1", "Scott")
         self.assertEqual(player.name, "Scott")
+
+    def test_add_password(self):
+        player = Player("1", "Scott")
+        plaintext_password = "this is correct password"
+        player.add_password(plaintext_password)
+        self.assertIsNotNone(player._hash_password)
+        self.assertTrue(len(player._hash_password) > 0)
+
+    def test_add_password_raises_error(self):
+        player = Player("1", "Scott")
+        with self.assertRaises(TypeError):
+            player.add_password(123)
+
+
+    def test_verify_password(self):
+        player = Player("1", "Scott")
+        plaintext_password = "this is correct password"
+        player.add_password(plaintext_password)
+        self.assertTrue(player.verify_password(plaintext_password))
+        self.assertFalse(player.verify_password("this is not correct password"))
+
+
 
 if __name__ == '__main__':
     unittest.main()
